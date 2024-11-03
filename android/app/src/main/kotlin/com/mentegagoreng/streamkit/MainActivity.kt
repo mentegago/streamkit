d184ddb2cb0f91eadc31aee8a68d790d2c3ef821
Mentega <mentegago@gmail.com>
2024-11-04 03:58:36 +0900

    Added Internet support

--- android/app/src/main/kotlin/com/mentegagoreng/streamkit/MainActivity.kt
+++ android/app/src/main/kotlin/com/mentegagoreng/streamkit/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.mentegagoreng.streamkit
 
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity;
 
-class MainActivity: FlutterActivity()
+class MainActivity: AudioServiceActivity()
---
+++ lib/services/app_audio_handler_service.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'package:audio_service/audio_service.dart';
+
+class AppAudioHandlerService extends BaseAudioHandler
+    with QueueHandler, SeekHandler {
+  Function()? onStop;
+  Function()? onPlay;
+
+  void setNotification({
+    required bool isPlaying,
+    String? username,
+  }) {
+    if (isPlaying) {
+      mediaItem.add(
+        MediaItem(
+          id: "id",
+          title: "StreamKit",
+          artist: username ?? "Account",
+        ),
+      );
+
+      playbackState.add(
+        PlaybackState(
+          processingState: AudioProcessingState.ready,
+          playing: true,
+          repeatMode: AudioServiceRepeatMode.one,
+        ),
+      );
+    } else {
+      playbackState.add(
+        PlaybackState(
+          playing: false,
+          processingState: AudioProcessingState.completed,
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<void> play() async {
+    if (onPlay != null) {
+      onPlay!();
+    }
+    return;
+  }
+
+  @override
+  Future<void> pause() async {
+    if (onStop != null) {
+      onStop!();
+    }
+    return super.pause();
+  }
+
+  @override
+  Future<void> stop() {
+    if (onStop != null) {
+      onStop!();
+    }
+    return super.stop();
+  }
+
+  @override
+  Future<void> seek(Duration position) async {
+    return;
+  }
+
+  @override
+  Future<void> skipToQueueItem(int index) async {
+    return;
+  }
+}
